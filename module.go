1ea02bd3edd2f5257b13275ddec9ae4e13708fdb
huy <huy@nodeset.io>
2024-11-21 13:07:34 -0800

    pr review

--- module.go
+++ module.go
@@ -3,8 +3,7 @@ package osha
 // Interface representing individual module snapshots that compose an entire Snapshot
 type IOshaModule interface {
 	GetName() string
-	GetRequirements()
 	Close() error
 	TakeSnapshot() (any, error)
-	RevertToSnapshot(name any) error
+	RevertToSnapshot(moduleState any) error
 }
